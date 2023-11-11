185bdacf1820c6b2d0effa793df4044592f2bd8e
Ahmad Shaleh Kurniawan <85729997+ahmdsk@users.noreply.github.com>
2023-11-11 13:25:14 +0700

    Update capacitive_soil_moisture_sensor_esp8266.ino

--- capacitive_soil_moisture_sensor_esp8266.ino
+++ capacitive_soil_moisture_sensor_esp8266.ino
@@ -1,3 +1,14 @@
+// Belajar Menghubungkan Sensor Kelembapan Tanah Dengan ESP8266
+// dan Sensor Tanah (Soil Misture Sensor v2.0)
+
+// Tabel Setup Alur Pin
+// Sensor | ESP8266
+// ----------------
+// GND    | G
+// VCC    | D8
+// AUOT   | AO
+
+// Kode
 const int sensor_pin = A0;
 
 void setup() {
